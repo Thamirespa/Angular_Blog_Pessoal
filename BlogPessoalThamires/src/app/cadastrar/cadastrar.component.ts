64e8ea074546fceba67fee5728d0c49e3f50382e
Thamirespa <thamires-araujopereira@hotmail.com>
2022-03-29 18:14:11 -0300

    Integração do Back-end e do Front-end

--- BlogPessoalThamires/src/app/cadastrar/cadastrar.component.ts
+++ BlogPessoalThamires/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+usuario: Usuario = new Usuario
+confirmarSenha: String
+tipoUsuario: String
+  constructor(
+    private authService:AuthService,
+    private router: Router
+   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+confirmeSenha(event:any){
+this.confirmarSenha = event.target.value
+}
+
+tipoUser(event:any){
+  this.tipoUsuario = event.target.value
+}
+
+cadastrarUsuario(){
+  if(this.usuario.senha != this.confirmarSenha){
+    alert("Suas senhas precisam ser iguais")
+    
+  }
+  else{
+    this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) =>{
+      this.usuario = resp
+      this.router.navigate(['/entrar'])
+      alert('Usuário cadastrado com sucesso')
+    })
+    
   }
 
+
+}
 }
---
+++ BlogPessoalThamires/src/app/model/Postagem.ts
@@ -0,0 +1,13 @@
+import { Data } from "popper.js";
+import { Tema } from "./Tema";
+import { Usuario } from "./Usuario";
+
+export class Postagem{
+    public id: number
+    public titulo: string
+    public texto: string
+    public data: Data
+    public usuario: Usuario
+    public tema: Tema
+    
+}
