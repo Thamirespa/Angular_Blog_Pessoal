856f5e3bd92109b507d6eb90b1572854a638b464
Thamirespa <thamires-araujopereira@hotmail.com>
2022-04-04 23:03:37 -0300

    Consumo tema

--- BlogPessoalThamires/src/app/model/Tema.ts
+++ BlogPessoalThamires/src/app/model/Tema.ts
@@ -3,7 +3,9 @@ import { Postagem } from "./Postagem";
 export class Tema{
     public id: number
 
-    public descricao: String
+    public descricao: string
+    
+    public nome: string
 
     public postagem: Postagem []
 
